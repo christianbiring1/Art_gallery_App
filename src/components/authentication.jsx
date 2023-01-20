4df07996cf2e70c704617581e70d1865fc340224
christian <christianbiringanine22@gmail.com>
2023-01-20 14:52:14 +0200

    set up firebase and install bootstrap

---
+++ src/components/authentication.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class LoginForm extends Component {
+  state = {}
+  render() {
+    return (
+      <h1>Login Form</h1>
+    );
+  }
+}
+
+export default LoginForm;
---
+++ src/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAv5tEqI1EuJL80nUFMoqgExrTPFh3vvBY",
+  authDomain: "art-gallery-caa2f.firebaseapp.com",
+  projectId: "art-gallery-caa2f",
+  storageBucket: "art-gallery-caa2f.appspot.com",
+  messagingSenderId: "951135626745",
+  appId: "1:951135626745:web:5a129a20f226a41ce00928"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
