ad9ec3a1a4dcf877affe4d04e651843b841d2eec
christian <christianbiringanine22@gmail.com>
2023-01-22 10:12:16 +0200

    get the posts from the server and displa them on the UI

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,7 +1,28 @@
-const Home = () => (
-  <div className="container">
-    Home
-  </div>
-);
+import React, { useState, useEffect } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../firebase-config';
+
+const Home = () => {
+  const [postsList, setPostsList] = useState([]);
+  const allPosts = collection(db, 'posts');
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const posts = await getDocs(allPosts);
+      const data = posts.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+      setPostsList(data);
+    };
+
+    getPosts();
+  }, [allPosts]);
+  return (
+    <div className="container">
+      <h1>Home</h1>
+      <div>
+        {postsList.map((post) => <div key={post.id}>{post.title}</div>)}
+      </div>
+    </div>
+  );
+};
 
 export default Home;
