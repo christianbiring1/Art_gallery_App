bbc67d3ee775ac298ebbd1e9707d1e81016a8fab
christian <christianbiringanine22@gmail.com>
2024-02-08 19:18:59 +0200

    Add MUI to make appealing the images

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,5 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { collection, getDocs } from 'firebase/firestore';
+import {
+  Card, CardHeader, CardMedia, CardContent, CardActions, Avatar, IconButton, Typography,
+} from '@mui/material'; //eslint-disable-line
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
+
 import { db } from '../firebase-config';
 
 const Home = () => {
@@ -20,24 +28,61 @@ const Home = () => {
       <h1>Gallery Feeds</h1>
       <div className="card-container">
         {postsList.map((post) => (
-          <div key={post.id} className="card--wrap">
-            <div className="card">
-              <img src={post.url} alt={`${post.author.name} post`} className="card-img" />
-              <div className="card-body">
-                <h5 className="card-title">{post.title}</h5>
-                <p className="card-text">{post.textarea}</p>
-                <p className="card-text">
-                  <small className="text-muted">
-                    Posted by:
-                    {' '}
-                  </small>
-                  <small className="text-muted" style={{ fontWeight: 'bold' }}>
-                    {post.author.name}
-                  </small>
-                </p>
-              </div>
-            </div>
-          </div>
+          <Card key={post.id} sx={{ fontSize: '16px'}} >
+            <CardHeader
+              avatar={(
+                <Avatar sx={{ bgcolor: 'GrayText' }} aria-label="recipe">
+                  R
+                </Avatar>
+              )}
+              action={(
+                <IconButton aria-label="settings">
+                  <MoreVertIcon />
+                </IconButton>
+              )}
+              title={post.title}
+              subheader="September 14, 2016"
+            />
+            <CardMedia
+              component="img"
+              height="194"
+              image={post.url}
+              alt={`${post.author.name} post`}
+            />
+            <CardContent>
+              <Typography variant="body2" color="text.secondary">
+                This impressive paella is a perfect party dish and a fun meal to cook
+                together with your guests. Add 1 cup of frozen peas along with the mussels,
+                if you like.
+              </Typography>
+            </CardContent>
+            <CardActions disableSpacing>
+              <IconButton aria-label="add to favorites">
+                <FavoriteIcon />
+              </IconButton>
+              <IconButton aria-label="share">
+                <ShareIcon />
+              </IconButton>
+            </CardActions>
+          </Card>
+          // <div key={post.id} className="card--wrap">
+          //   <div className="card">
+          //     <img src={post.url} alt={`${post.author.name} post`} className="card-img" />
+          //     <div className="card-body">
+          //       <h5 className="card-title">{post.title}</h5>
+          //       <p className="card-text">{post.textarea}</p>
+          //       <p className="card-text">
+          //         <small className="text-muted">
+          //           Posted by:
+          //           {' '}
+          //         </small>
+          //         <small className="text-muted" style={{ fontWeight: 'bold' }}>
+          //           {post.author.name}
+          //         </small>
+          //       </p>
+          //     </div>
+          //   </div>
+          // </div>
         ))}
       </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
+/* eslint-disable */
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
